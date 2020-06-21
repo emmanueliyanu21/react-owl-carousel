ab9342d7b9e3e955bd76d4e69e09bd51cacf841d
emmanueliyanu21 <emmanueliyanu2012@gmail.com>
2020-06-21 23:33:54 +0100

    everything working

--- src/components/Owldemo1.js
+++ src/components/Owldemo1.js
@@ -3,6 +3,7 @@ import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import './owl.css';
+import './owl.js'
 
 export class Owldemo1 extends Component {
     render() {
